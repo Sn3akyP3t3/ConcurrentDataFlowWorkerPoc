87b7bdbbf0911f7bed39c93875bdce7a555356d2
Sn3akyP3t3 <git_repos.cy04t@slmail.me>
2024-09-04 20:11:23 -0500

    Fixed the build.

--- ConcurrentDataflowWorkerPoc/Worker.cs
+++ ConcurrentDataflowWorkerPoc/Worker.cs
@@ -3,19 +3,19 @@ namespace ConcurrentDataflowWorkerPoc
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly IServiceRunner _serviceRunner;
+        private readonly IDataWorkflowService _dataWorkflowService;
 
-        public Worker(ILogger<Worker> logger, IServiceRunner serviceRunner)
+        public Worker(ILogger<Worker> logger, IDataWorkflowService dataWorkflowService)
         {
             _logger = logger;
-            _serviceRunner = serviceRunner;
+            _dataWorkflowService = dataWorkflowService;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                await _serviceRunner.DoProcess();
+                await _dataWorkflowService.DoProcess();
 
                 await Task.Delay(10000, stoppingToken);
             }
